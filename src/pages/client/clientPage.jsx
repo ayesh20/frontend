02a07394f7e5e78594e5867ff92e9ec41ab097ba
ayesh jayashan <135942697+ayesh20@users.noreply.github.com>
2025-07-23 09:55:55 +0530

    day 17 update

--- src/pages/client/clientPage.jsx
+++ src/pages/client/clientPage.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "../../components/header";
+import ProductsPage from "./productpage";
 
 export default function ClientWebPage() {
 	return <div className="w-full h-screen max-h-screen">
@@ -7,7 +8,7 @@ export default function ClientWebPage() {
         <div className="w-full h-[calc(100%-100px)] bg-yellow-400">
             <Routes path="/">
                 <Route path="/" element={<h1 className="text-3xl text-center">Welcome to the Home Page</h1>}/>
-                <Route path="/products" element={<h1 className="text-3xl text-center">Products Page</h1>}/>
+                <Route path="/products" element={<ProductsPage></ProductsPage>}/>
                 <Route path="/reviews" element={<h1 className="text-3xl text-center">Reviews Page</h1>}/>
                 <Route path="/about-us" element={<h1 className="text-3xl text-center">About Us Page</h1>}/>
                 <Route path="/contact-us" element={<h1 className="text-3xl text-center">Contact Us Page</h1>}/>
